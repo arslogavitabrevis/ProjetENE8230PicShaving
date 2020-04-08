b94ef7ad658cf6811c2c6a4baaa01d77b28f6c2d
Raphael <raphael1grenier@gmail.com>
2020-04-07 22:25:05 -0400

    Add a document in the result to store cost value

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import pulp as plp
 import numpy as np
 from os import path, makedirs
+from datetime import datetime
 from constraints import constraints
 from objectiveFunction import objFct
 from decisionVariables import decisionVariables
@@ -23,6 +24,11 @@ print("Cout ={:,.2f} ".format(plp.value(picShavingProb.objective)))
 
 if not path.exists('./Results'):
     makedirs('Results')
+
+with open("Results/GeneralInfo.txt", 'w') as f:
+    f.write("{}\n".format(datetime.now()))
+    f.write("Status: {}\n".format(plp.LpStatus[picShavingProb.status]))
+    f.write("Cout ={:,.2f}\n".format(plp.value(picShavingProb.objective)))
 
 with open("Results/AllVariableValue.txt", 'w') as f:
     f.writelines(list(map(lambda v: "{} = {:,.1f}\n".format(v.name, v.varValue),
